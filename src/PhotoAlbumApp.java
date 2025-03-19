362e9feb538b222489797b8628376279150df54d
marlonhuynh <bettytuduyen@gmail.com>
2025-03-19 04:27:00 -0700

    updated dates

--- src/PhotoAlbumApp.java
+++ src/PhotoAlbumApp.java
@@ -1,3 +1,12 @@
+/**
+ * PhotoAlbumApp.java
+ *
+ * Description: Creates a photo album app using the MVC pattern and implementing Strategy pattern.
+ *
+ * @author  Marlon Huynh
+ * @version 1.0, 3/29/2025
+ */
+
 public class PhotoAlbumApp {
     public static void main(String[] args) {
         PhotoAlbumModel model = new PhotoAlbumModel();
--- src/PhotoAlbumModel.java
+++ src/PhotoAlbumModel.java
@@ -1,12 +1,29 @@
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Iterator;
 
 public class PhotoAlbumModel {
     private List<String> photos;
-    public PhotoAlbumModel() {
-        photos = new ArrayList<>();
-    }
+
+    public PhotoAlbumModel() { photos = new ArrayList<>();  }
     public List<String> getPhotos() { return photos; }
     public void addPhoto(String photo) { photos.add(photo); }
     public void removePhoto(String photo) { photos.remove(photo); }
+
+    // Iterator implementation
+    public Iterator<String> iterator() { return new PhotoAlbumIterator(); }
+    private class PhotoAlbumIterator implements Iterator<String> {
+        private int index = 0; // Specific to each iterator!
+        @Override
+        public boolean hasNext() {
+            return index < photos.size();
+        }
+        @Override
+        public String next() {
+            if (hasNext()) {
+                return photos.get(index++);
+            }
+            throw new IndexOutOfBoundsException("No more photos.");
+        }
+    }
 }
